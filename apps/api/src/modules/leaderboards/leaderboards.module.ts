694b55f00afee0daaed304537721c79cab126898
Luke Siedle <ljsiedle@gmail.com>
2021-03-21 12:25:43 +0200

    API changes for leaderboards

--- apps/api/src/modules/leaderboards/leaderboards.module.ts
+++ apps/api/src/modules/leaderboards/leaderboards.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { LeaderboardsService } from './leaderboards.service'
 import { LeaderboardsController } from './leaderboards.controller'
+import { AuthModule } from '../auth/auth.module'
 
 @Module({
+  imports: [AuthModule],
   controllers: [LeaderboardsController],
   providers: [LeaderboardsService]
 })
