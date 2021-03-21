c8541b958c9e82308f8b15c23fbfa0a70b030dfc
Luke Siedle <ljsiedle@gmail.com>
2021-03-21 12:32:45 +0200

    Swagger api maintenance

--- apps/api/src/modules/activities/dto/create-activity.dto.ts
+++ apps/api/src/modules/activities/dto/create-activity.dto.ts
@@ -1,22 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsUrl, IsLatLong, IsArray, IsOptional } from 'class-validator'
 import { Image } from '../../images/entities/image.entity'
 
 export class CreateActivityDto {
+  @ApiProperty()
   name: string
+
+  @ApiProperty()
   description: string
+
+  @ApiProperty()
   date: string
+
+  @ApiProperty()
   meeting_point_text: string
 
+  @ApiProperty()
   @IsUrl()
   organizer_url: string
 
+  @ApiProperty()
   @IsLatLong()
   meeting_point: string
 
+  @ApiProperty()
   @IsArray()
   @IsOptional()
   images?: Image[]
 
+  @ApiProperty()
   cost?: string
+
+  @ApiProperty()
   organizer_name?: string
 }
