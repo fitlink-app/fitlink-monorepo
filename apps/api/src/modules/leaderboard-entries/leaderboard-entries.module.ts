959dfd6d138e7ca2db2fea77962d1ffcb128d45f
Luke Siedle <ljsiedle@gmail.com>
2021-03-21 12:24:41 +0200

    Fix issues with leaderboard entries and jwt/auth changes

--- apps/api/src/modules/leaderboard-entries/leaderboard-entries.module.ts
+++ apps/api/src/modules/leaderboard-entries/leaderboard-entries.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { LeaderboardEntriesService } from './leaderboard-entries.service'
 import { LeaderboardEntriesController } from './leaderboard-entries.controller'
 import { LeaderboardEntry } from './entities/leaderboard-entry.entity'
+import { AuthModule } from '../auth/auth.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LeaderboardEntry])],
+  imports: [AuthModule, TypeOrmModule.forFeature([LeaderboardEntry])],
   controllers: [LeaderboardEntriesController],
   providers: [LeaderboardEntriesService],
   exports: [
