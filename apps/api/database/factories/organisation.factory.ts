43b21ce0b04269fe475ad7a70312d7544d511f09
Luke Siedle <ljsiedle@gmail.com>
2021-03-22 08:27:10 +0200

    Changes to organisations seed

--- apps/api/database/factories/organisation.factory.ts
+++ apps/api/database/factories/organisation.factory.ts
@@ -4,12 +4,6 @@ import Faker from 'faker'
 
 define(Organisation, (faker: typeof Faker) => {
   const org = new Organisation()
-  org.billing_address_1 = faker.address.streetAddress()
-  org.billing_address_2 = faker.address.secondaryAddress()
-  org.billing_city = faker.address.city()
-  org.billing_country = faker.address.country()
-  org.billing_state = faker.address.state()
-  org.billing_country_code = faker.address.countryCode()
   org.name = faker.company.companyName()
   return org
 })
