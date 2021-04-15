7c9b6c718b99f8f90a5a421539081ac760e31a59
Luke Siedle <ljsiedle@gmail.com>
2021-04-15 13:24:34 +0200

    Fix pagination bugs, add a test for leaderboard entries

--- apps/api/src/modules/followings/followings.service.ts
+++ apps/api/src/modules/followings/followings.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable, BadRequestException } from '@nestjs/common'
 import { CreateFollowingDto } from './dto/create-following.dto'
 import { InjectRepository } from '@nestjs/typeorm'
@@ -19,116 +18,105 @@ export class FollowingsService {
    * @param userId
    * @param createFollowingDto
    */
-    async create(
-        userId: string,
-        createFollowingDto: CreateFollowingDto
-      ): Promise<Following> {
-
-      if (createFollowingDto.targetId === userId) {
-        throw new BadRequestException('user and target id should be different')
-      }
-
-      const userToFollow = new User()
-      userToFollow.id = createFollowingDto.targetId
-
-      const me = new User()
-      me.id = userId
-
-      const following = new Following()
-      following.following = userToFollow
-      following.follower = me
-
-      const result = await this.followingRepository.findOne(following)
-
-      return await this.followingRepository.save({
-        ...result,
-        ...following
-      })
+  async create(
+    userId: string,
+    createFollowingDto: CreateFollowingDto
+  ): Promise<Following> {
+    if (createFollowingDto.targetId === userId) {
+      throw new BadRequestException('user and target id should be different')
     }
+
+    const userToFollow = new User()
+    userToFollow.id = createFollowingDto.targetId
+
+    const me = new User()
+    me.id = userId
+
+    const following = new Following()
+    following.following = userToFollow
+    following.follower = me
+
+    const result = await this.followingRepository.findOne(following)
+
+    return await this.followingRepository.save({
+      ...result,
+      ...following
+    })
+  }
 
   /**
    * Find all followings by user (followerId), with pagination options
    * @param userId
    * @param options
    */
-    async findAllFollowing(
-      userId: string,
-      options: PaginationOptionsInterface
-    ): Promise<Pagination<Following>> {
+  async findAllFollowing(
+    userId: string,
+    options: PaginationOptionsInterface
+  ): Promise<Pagination<Following>> {
+    const me = new User()
+    me.id = userId
 
-      const me = new User()
-      me.id = userId
+    const [results, total] = await this.followingRepository.findAndCount({
+      where: { follower: { id: userId } },
+      relations: ['following'],
+      take: options.limit,
+      skip: options.page * options.limit
+    })
 
-      const [results, total] = await this.followingRepository.findAndCount(
-        {
-          where: {follower: {id: userId}},
-          relations: ['following'],
-          take: options.limit,
-          skip: options.page
-        }
-      )
-
-      return new Pagination<Following>({
-        results,
-        total
-      })
-    }
+    return new Pagination<Following>({
+      results,
+      total
+    })
+  }
 
   /**
    * Find all followers by user (followingId), with pagination options
    * @param userId
    * @param options
    */
-     async findAllFollowers(
-      userId: string,
-      options: PaginationOptionsInterface
-    ): Promise<Pagination<Following>> {
+  async findAllFollowers(
+    userId: string,
+    options: PaginationOptionsInterface
+  ): Promise<Pagination<Following>> {
+    const me = new User()
+    me.id = userId
 
-      const me = new User()
-      me.id = userId
+    const [results, total] = await this.followingRepository.findAndCount({
+      where: { following: { id: userId } },
+      relations: ['following'],
+      take: options.limit,
+      skip: options.page * options.limit
+    })
 
-      const [results, total] = await this.followingRepository.findAndCount(
-        {
-          where: {following: {id: userId}},
-          relations: ['following'],
-          take: options.limit,
-          skip: options.page
-        }
-      )
-
-      return new Pagination<Following>({
-        results,
-        total
-      })
-    }
+    return new Pagination<Following>({
+      results,
+      total
+    })
+  }
 
   /**
    * Find a specific following entry
    */
-    async findOne(following) {
-      return await this.followingRepository.findOne(following)
-    }
-
+  async findOne(following) {
+    return await this.followingRepository.findOne(following)
+  }
 
   /**
    * Deletes the following
    * @param userId
    * @param targetId
    */
-    async remove(
-      userId: string,
-      targetId: string
-    ) {
-      const userToFollow = new User()
-      userToFollow.id = targetId
+  async remove(userId: string, targetId: string) {
+    const userToFollow = new User()
+    userToFollow.id = targetId
 
-      const me = new User()
-      me.id = userId
+    const me = new User()
+    me.id = userId
 
-      const following = new Following()
-      following.following = userToFollow
-      following.follower = me
+    const following = new Following()
+    following.following = userToFollow
+    following.follower = me
 
-      return await this.followingRepository.delete(following)
-    }
+    return await this.followingRepository.delete(following)
+  }
 }
