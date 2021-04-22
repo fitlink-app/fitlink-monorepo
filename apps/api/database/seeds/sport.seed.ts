e5ee99fb388572eba0a2247052ff798ca9981a82
Luke Siedle <ljsiedle@gmail.com>
2021-04-22 10:40:30 +0200

    Fixes sport seeds issue with conditional

--- apps/api/database/seeds/sport.seed.ts
+++ apps/api/database/seeds/sport.seed.ts
@@ -1,23 +1,29 @@
 import { Seeder, Factory } from 'typeorm-seeding'
+import { Connection } from 'typeorm'
 import { Sport } from '../../src/modules/sports/entities/sport.entity'
 
 export default class CreateSports implements Seeder {
-  public async run(factory: Factory): Promise<any> {
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    const sportRepository = connection.getRepository(Sport)
     /**
      * Create sports list
      */
-    await factory(Sport)().create({
-      name: 'Running',
-      name_key: 'running',
-      singular: 'run',
-      plural: 'runs'
-    })
+    if (!(await sportRepository.findOne({ name_key: 'running' }))) {
+      await factory(Sport)().create({
+        name: 'Running',
+        name_key: 'running',
+        singular: 'run',
+        plural: 'runs'
+      })
+    }
 
-    await factory(Sport)().create({
-      name: 'Cycling',
-      name_key: 'cycling',
-      singular: 'ride',
-      plural: 'rides'
-    })
+    if (!(await sportRepository.findOne({ name_key: 'cycling' }))) {
+      await factory(Sport)().create({
+        name: 'Cycling',
+        name_key: 'cycling',
+        singular: 'ride',
+        plural: 'rides'
+      })
+    }
   }
 }
