c82e74d346d105897e6517fc0eb56a5c95c83977
Sohail Haqyar <haqyarsohail@gmail.com>
2021-05-11 07:45:04 +0300

    feature/UserSettingsCRUD

--- apps/api/src/modules/users-settings/users-settings.service.ts
+++ apps/api/src/modules/users-settings/users-settings.service.ts
@@ -1,26 +1,66 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { User } from '../users/entities/user.entity'
 import { CreateUsersSettingDto } from './dto/create-users-setting.dto'
 import { UpdateUsersSettingDto } from './dto/update-users-setting.dto'
+import { UsersSetting } from './entities/users-setting.entity'
 
 @Injectable()
 export class UserSettingsService {
-  create(createUsersSettingDto: CreateUsersSettingDto) {
-    return 'This action adds a new userSetting'
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    @InjectRepository(UsersSetting)
+    private userSettingsRepository: Repository<UsersSetting>
+  ) {}
+  async create(createUsersSettingDto: CreateUsersSettingDto, userId: string) {
+    const userSettings = await this.userSettingsRepository.save(
+      this.userSettingsRepository.create({
+        ...createUsersSettingDto,
+        user: { id: userId }
+      })
+    )
+    return userSettings
   }
 
-  findAll() {
-    return `This action returns all userSettings`
+  async findOne(id: string) {
+    const user = await this.userRepository.findOne(id, {
+      relations: ['settings']
+    })
+    return user.settings
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} userSetting`
+  async update(id: string, updateUsersSettingDto: UpdateUsersSettingDto) {
+    const user = await this.userRepository.findOne(id, {
+      relations: ['settings']
+    })
+
+    const newUserSettings = await this.userSettingsRepository.save({
+      user,
+      ...user.settings,
+      ...updateUsersSettingDto
+    })
+
+    delete newUserSettings.user
+    return newUserSettings
   }
 
-  update(id: number, updateUsersSettingDto: UpdateUsersSettingDto) {
-    return `This action updates a #${id} userSetting`
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} userSetting`
+  async remove(id: string) {
+    const user = await this.userRepository.findOne(id, {
+      relations: ['settings']
+    })
+    await this.userRepository
+      .createQueryBuilder()
+      .relation(User, 'settings')
+      .of(id)
+      .set(null)
+    const deleteResults = await this.userSettingsRepository.delete(
+      user.settings.id
+    )
+    return {
+      deleteResults,
+      user
+    }
   }
 }
