b7caa539b9aac3d0e0f1f54b58127240f7ff0769
Luke Siedle <ljsiedle@gmail.com>
2021-05-17 15:23:04 +0200

    Adds Storybook support

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../apps/storybook/**/*.stories.mdx",
+    "../apps/storybook/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
+  ]
+}
