0e9590487007543cfb626ba91ea7a7b1ec76c930
Brett Meyer <hello@brettmeyer.net>
2021-06-01 11:54:01 +0200

    Page holders

---
+++ apps/admin/src/pages/activities.tsx
@@ -0,0 +1,12 @@
+import Dashboard from '../components/layouts/Dashboard'
+
+export default function components() {
+
+  return (
+    <Dashboard
+      title="Activities"
+      >
+      <h1 className="light">Your activities</h1>
+    </Dashboard>
+  )
+}
---
+++ apps/admin/src/pages/knowledge-base.tsx
@@ -0,0 +1,12 @@
+import Dashboard from '../components/layouts/Dashboard'
+
+export default function components() {
+
+  return (
+    <Dashboard
+      title="Knowledge base"
+      >
+      <h1 className="light">Knowledge base</h1>
+    </Dashboard>
+  )
+}
