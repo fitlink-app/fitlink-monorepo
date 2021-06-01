5d46b987ab71712a039e74728865e2bfb2d9c6f3
Brett Meyer <hello@brettmeyer.net>
2021-06-01 11:54:13 +0200

    Updated summary

--- apps/admin/src/pages/dashboard.tsx
+++ apps/admin/src/pages/dashboard.tsx
@@ -5,12 +5,9 @@ import IconFriends from '../components/icons/IconFriends'
 import IconSteps from '../components/icons/IconSteps'
 import IconSleep from '../components/icons/IconSleep'
 import IconStairs from '../components/icons/IconStairs'
-import IconRun from '../components/icons/IconRun'
-import IconBike from '../components/icons/IconBike'
 import IconYoga from '../components/icons/IconYoga'
-import IconRewards from '../components/icons/IconRewards'
-import CircleProgess from '../components/charts/CircleProgress'
 import ProgressChart from '../components/charts/ProgressChart'
+import IconWater from '../components/icons/IconWater'
 
 export default function components() {
 
@@ -32,60 +29,63 @@ export default function components() {
           <Card className="p-3 card--stretch">
             <h2 className="h5 color-light-grey">How is your team doing?</h2>
             <div className="row mt-4">
-              <div className="col-3 text-center">
+              <div className="col-4 text-center">
                 <ProgressChart
                   progress={ 85 }
                   icon={ <IconFriends /> }
-                  value="22/26"
+                  value="22"
+                  goal="26"
                   label="Active users"
                 />
               </div>
-              <div className="col-3 text-center">
+              <div className="col-4 text-center">
                 <ProgressChart
                   progress={ 81 }
                   icon={ <IconSteps /> }
-                  value="8134"
+                  value="8,134"
+                  goal="10,000"
                   label="Average daily steps"
                 />
               </div>
-              <div className="col-3 text-center">
+              <div className="col-4 text-center">
                 <ProgressChart
                   progress={ 75 }
                   icon={ <IconSleep /> }
                   value="6"
+                  goal="8"
                   label="Average hours slept"
-                />
-              </div>
-              <div className="col-3 text-center">
-                <ProgressChart
-                  progress={ 33 }
-                  icon={ <IconYoga /> }
-                  value="3"
-                  label="Average mindful minutes"
                 />
               </div>
             </div>
             <div className="row mt-2">
-              <div className="col-3 text-center">
+              <div className="col-4 text-center">
+                <ProgressChart
+                  progress={ 33 }
+                  icon={ <IconYoga /> }
+                  value="3"
+                  goal="10"
+                  label="Average mindful minutes"
+                />
+              </div>
+              <div className="col-4 text-center">
                 <ProgressChart
                   progress={ 100 }
                   icon={ <IconStairs /> }
                   value="14"
+                  goal="11"
                   label="Average floors climbed"
                 />
               </div>
-              <div className="col-3 text-center">
-                <IconRun width="32px" height="32px" />
-                <h3 className="p">Distance Run</h3>
+              <div className="col-4 text-center">
+                <ProgressChart
+                  progress={ 50 }
+                  icon={ <IconWater /> }
+                  value="1"
+                  goal="2L"
+                  label="Average water intake"
+                />
               </div>
-              <div className="col-3 text-center">
-                <IconBike width="32px" height="32px" />
-                <h3 className="p">Distance Cycled</h3>
-              </div>
-              <div className="col-3 text-center">
-                <IconRewards width="32px" height="32px" />
-                <h3 className="p">Rewards Claimed</h3>
-              </div>
+              
             </div>
           </Card>
         </div>
