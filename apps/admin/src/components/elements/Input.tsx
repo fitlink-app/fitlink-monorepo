eb633a02d479a2006226bf8186c5655b3d8a8720
Brett Meyer <hello@brettmeyer.net>
2021-06-03 13:06:25 +0200

    Added textarea and internal state

--- apps/admin/src/components/elements/Input.tsx
+++ apps/admin/src/components/elements/Input.tsx
@@ -1,35 +1,67 @@
+import clsx from "clsx"
+import { useEffect, useState } from "react"
+
 export type InputProps = {
-  type?: 'text' | 'number' | 'email' | 'tel' | 'textfield'
+  type?: 'text' | 'number' | 'email' | 'tel' | 'textarea'
   label: string
   placeholder?: string
   name: string
   value?: any
-  onChange?: () => void
+  changed?: (e:any) => void
+  rows?: number
+  className?: string
 }
 
 export default function Input({
+  className,
   type = 'text',
   label,
   placeholder = '',
   name,
   value = '',
-  onChange = () => null
+  changed = (e:any) => null,
+  rows = 5
 }: InputProps) {
 
+  const [val, setVal] = useState(value)
+  const classes = clsx({
+    'input-block': true,
+    [className]: className
+  })
+
+/*   useEffect(() => {
+    setVal(value)
+  }, [value]) */
+
+  const handleChange = (e) => {
+    changed(e.target.value)
+    setVal(e.target.value)
+  }
+
   return (
-    <div className="input-block">
+    <div className={classes}>
       <label
         htmlFor={name}
         >
         {label}
       </label>
-      <input
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        />
+      { type !== 'textarea' ?
+        <input
+          type={type}
+          name={name}
+          value={val}
+          onChange={ (e) => handleChange(e) }
+          placeholder={placeholder}
+          />
+      :
+        <textarea
+          name={name}
+          value={val}
+          onChange={ (e) => handleChange(e) }
+          placeholder={placeholder}
+          rows={rows}
+        ></textarea>
+      }
     </div>
   )
 }
