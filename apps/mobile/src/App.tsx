656ed4011f7964824f1dfb8fd268dd780cded249
Martin <martin999b@gmail.com>
2021-06-08 15:15:45 +0200

    add typing and default theme for styled components

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 
 import Router from './routes/router';
+import ThemeProvider from './theme/ThemeProvider';
 const App = () => {
   return (
-    <SafeAreaView>
-      <Router />
-    </SafeAreaView>
+    <ThemeProvider>
+      <SafeAreaView>
+        <Router />
+      </SafeAreaView>
+    </ThemeProvider>
   );
 };
 
