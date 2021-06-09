2ba3558037a00efe24adad8b72e62d65220d61b2
Brett Meyer <hello@brettmeyer.net>
2021-06-09 15:11:26 +0200

    arrow icons

---
+++ apps/admin/src/components/icons/IconArrowRight.tsx
@@ -0,0 +1,9 @@
+import Icon, { IconProps } from './Icon'
+
+export default function IconArrowRight(props:IconProps) {
+  return (
+    <Icon {...props}>
+      <path fill="currentColor" d="M21.542 16.664l-9.203 9.062c-0.367 0.367-0.961 0.367-1.328 0l-0.555-0.555c-0.367-0.367-0.367-0.961 0-1.328l7.992-7.843-7.984-7.843c-0.367-0.367-0.367-0.961 0-1.328l0.555-0.555c0.367-0.367 0.961-0.367 1.328 0l9.203 9.062c0.359 0.367 0.359 0.961-0.008 1.328z" />
+    </Icon>
+  )
+}
