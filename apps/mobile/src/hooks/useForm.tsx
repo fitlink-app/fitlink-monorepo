a34df9177e6db57e2066ca79a7af79503151c282
Martin <martin999b@gmail.com>
2021-06-11 12:33:25 +0200

    improve form hook

--- apps/mobile/src/hooks/useForm.tsx
+++ apps/mobile/src/hooks/useForm.tsx
@@ -1,40 +1,64 @@
 import {useState} from 'react';
+import {RequestError} from '@api';
 
 export function useForm<T, K extends keyof T>(initialValues: T) {
   const [values, setValues] = useState(initialValues);
-  const [errors, setErrors] = useState<Partial<T>>({} as T);
+  const [fieldErrors, setFieldErrors] = useState<Partial<T>>({} as T);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [isSubmitting, setSubmitting] = useState(false);
 
   /** Set field value */
   const handleChange = (name: K) => (value: typeof values[K]) => {
     setValues(values => ({...values, [name]: value}));
 
-    if (errors[name]) {
-      setErrors({...errors, [name]: ''});
+    setErrorMessage(null);
+
+    if (fieldErrors[name]) {
+      setFieldErrors({...fieldErrors, [name]: ''});
     }
   };
 
-  /** Submits form */
-  const handleSubmit = (callback: (data: T) => Promise<void>) => async () => {
-    setSubmitting(true);
-    setErrors({});
+  /**
+   * Submits the form by invoking the provided callback
+   * If the promise returns a RequestError, the error values will be stored in the state
+   *
+   * @param callback
+   * @returns
+   */
+  const handleSubmit =
+    (
+      callback: (data: T) => Promise<RequestError | undefined> | Promise<void>,
+    ) =>
+    async () => {
+      setSubmitting(true);
+      setFieldErrors({});
 
-    try {
-      await callback(values);
-    } catch (e) {
-      // TODO: Handle form errors
-      console.warn('TODO: Handle form errors', e.message);
-    }
+      try {
+        const requestError = await callback(values);
 
-    setSubmitting(false);
-  };
+        // Process request error (if provided) and store values in state
+        if (requestError) {
+          const {message, fields} = requestError;
+          if (message) setErrorMessage(message);
+          if (fields) setFieldErrors(fields as unknown as Partial<T>);
+        }
+      } catch (e) {
+        console.warn(
+          'Something went wrong, we should not throw here at all: ',
+          e.message,
+        );
+      }
+
+      setSubmitting(false);
+    };
 
   return {
     handleChange,
     handleSubmit,
     isSubmitting,
-    errors,
-    setErrors,
+    errorMessage,
+    setErrorMessage,
+    fieldErrors,
     values,
   };
 }
