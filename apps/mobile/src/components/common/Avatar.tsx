047705612ac288fa5a9a78c2ef26886e462d6643
Martin <martin999b@gmail.com>
2021-06-14 13:51:22 +0200

    improves Avatar

--- apps/mobile/src/components/common/Avatar.tsx
+++ apps/mobile/src/components/common/Avatar.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import {ImageSourcePropType, ImageProps} from 'react-native';
+import {ImageProps} from 'react-native';
 
 const defaultAvatar = require('../../../assets/images/default_avatar.jpg');
+const defaultSize = 80;
 
-const StyledImage = styled.Image<AvatarProps & ImageProps>(({size}) => ({
-  height: size,
-  width: size,
-  borderRadius: size * 2,
-}));
+const StyledImage = styled.Image<AvatarProps & ImageProps>(
+  ({size = defaultSize}) => ({
+    height: size,
+    width: size,
+    borderRadius: size * 2,
+  }),
+);
 
 interface AvatarProps extends Omit<ImageProps, 'source'> {
   /** Will use default avatar if unset */
-  source?: ImageSourcePropType;
-  size: number;
+  url?: string;
+  size?: number;
 }
 
 export const Avatar: React.FC<AvatarProps> = props => {
-  const {source = defaultAvatar, ...rest} = props;
-  return <StyledImage source={source} {...rest} />;
+  const {url, size = defaultSize, ...rest} = props;
+  return <StyledImage source={url ? {uri: url} : defaultAvatar} {...rest} />;
 };
