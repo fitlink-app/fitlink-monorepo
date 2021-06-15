1a155df1c9c333cd805c7425d3d237b11e012260
Martin <martin999b@gmail.com>
2021-06-15 09:11:16 +0200

    create GoalTracker widget

---
+++ apps/mobile/src/components/profile/GoalTracker/GoalTracker.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {Card, Label} from '../../common';
+import styled from 'styled-components/native';
+import {Goal} from './components/Goal';
+
+const Wrapper = styled(Card)({
+  paddingVertical: 10,
+});
+
+const Title = styled(Label).attrs(() => ({
+  type: 'caption',
+  bold: true,
+}))({
+  marginLeft: 15,
+});
+
+const WidgetContainer = styled.ScrollView.attrs(() => ({
+  horizontal: true,
+  overScrollMode: 'never',
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingHorizontal: 15,
+    justifyContent: 'space-between',
+  },
+}))({
+  marginTop: 12,
+});
+
+const StyledGoal = styled(Goal)({
+  marginRight: 15,
+});
+
+type Tracker = {
+  /** Whether this tracker is enabled */
+  enabled: boolean;
+
+  /** Identifier of the goal e.g.: "steps" */
+  identifier: string;
+
+  /** Current value and target value of the goal */
+  goal: Goal;
+
+  /** Icon name */
+  icon: string;
+};
+
+interface GoalTrackerProps {
+  trackers: Tracker[];
+}
+
+export const GoalTracker = ({trackers}: GoalTrackerProps) => {
+  const renderWidgets = () => {
+    const sortedTrackers: Tracker[] = [...trackers];
+
+    sortedTrackers.sort((a, b) =>
+      a.enabled === b.enabled ? 0 : a.enabled ? -1 : 1,
+    );
+
+    return sortedTrackers.map(({goal, icon, enabled, identifier}) => (
+      <StyledGoal key={identifier} {...{goal, icon}} disabled={!enabled} />
+    ));
+  };
+
+  return (
+    <Wrapper>
+      <Title>Today's Goals</Title>
+      <WidgetContainer>{renderWidgets()}</WidgetContainer>
+    </Wrapper>
+  );
+};
--- apps/mobile/src/components/profile/GoalTracker/index.ts
+++ apps/mobile/src/components/profile/GoalTracker/index.ts
@@ -1,2 +1,1 @@
-export * from './UserWidget';
 export * from './GoalTracker';
