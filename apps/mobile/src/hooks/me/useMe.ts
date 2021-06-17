b6c1be0f534dffce5ecc9605ef29213986490f83
Martin <martin999b@gmail.com>
2021-06-17 13:19:11 +0200

    adds options parameter to useMe hook

--- apps/mobile/src/hooks/me/useMe.ts
+++ apps/mobile/src/hooks/me/useMe.ts
@@ -1,15 +1,12 @@
-import {useQuery} from 'react-query';
+import {useQuery, UseQueryOptions, QueryKey} from 'react-query';
 import {QueryKeys} from '@query';
 import api from '@api';
 import {User} from '@fitlink/api/src/modules/users/entities/user.entity';
 
-export function useMe() {
-  return useQuery<User, Error>(QueryKeys.Me, async () => {
-    try {
-      const response = await api.get<User>('/me');
-      return response;
-    } catch (err) {
-      throw Error(err);
-    }
-  });
+export function useMe(options?: UseQueryOptions<User, Error, User, QueryKey>) {
+  return useQuery<User, Error>(
+    QueryKeys.Me,
+    () => api.get<User>('/me'),
+    options,
+  );
 }
