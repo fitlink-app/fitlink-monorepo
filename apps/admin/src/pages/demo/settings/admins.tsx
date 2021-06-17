bec96f40f186fc1679b39846518bb41927bf43ae
Brett Meyer <hello@brettmeyer.net>
2021-06-17 11:57:48 +0200

    settings sub pages

---
+++ apps/admin/src/pages/demo/settings/admins.tsx
@@ -0,0 +1,9 @@
+import Dashboard from '../../../components/layouts/Dashboard'
+
+export default function components() {
+  return (
+    <Dashboard title="Settings Admins">
+      <h1 className="light">Admin access</h1>
+    </Dashboard>
+  )
+}
