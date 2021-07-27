3d2f5bb0ec42b0501ec306ee2e1939b4904f3339
Brett Meyer <hello@brettmeyer.net>
2021-07-27 13:18:56 +0200

    cleaned up unused code

--- apps/admin/src/components/layouts/Dashboard.tsx
+++ apps/admin/src/components/layouts/Dashboard.tsx
@@ -15,8 +15,7 @@ let hydrated = false
 export default function Dashboard({
   children,
   title = 'Fitlink',
-  description = '',
-  //image = '/img/fitlink-og.png'
+  description = ''
 }:DashboardProps) {
 
   const hydratedRef = useRef(false);
@@ -42,22 +41,8 @@ export default function Dashboard({
         <meta name="google-site-verification" content="-SABtmBSc5R69B2lYHBUOHlVLnEoPu8qAOk3VZxZhBk" />
         <meta name="description" content={description} />
         <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
-        {/* <meta property="og:locale" content="en_GB" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:url" content={url} />
-        <meta property="og:site_name" content="Fitlink" />
-        <meta property="article:publisher" content="https://www.facebook.com/fitlinkapp" />
-        <meta property="article:modified_time" content="2021-01-22T15:24:12+00:00" />
-        <meta property="og:image" content={image} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@fitlinkapp" /> */}
         <link rel="canonical" href={url} />
         <link rel="shortcut icon" href="/img/favicon.png" />
-
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" /> 
         <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"></link>
         <link rel="stylesheet" media={!hydrated ? "print" : "all"} href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" />
