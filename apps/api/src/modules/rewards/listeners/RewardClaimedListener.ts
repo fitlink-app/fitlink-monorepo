103fde83edba77ed99ff9df039f4a033c742f8ba
Sohail Haqyar <haqyarsohail@gmail.com>
2021-08-17 18:05:11 +0300

    feature/RewardClaimed

---
+++ apps/api/src/modules/rewards/listeners/RewardClaimedListener.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common'
+import { OnEvent } from '@nestjs/event-emitter'
+import { InjectRepository } from '@nestjs/typeorm'
+import { tryAndCatch } from '../../../../src/helpers/tryAndCatch'
+import { Repository } from 'typeorm'
+import {
+  FeedItemCategory,
+  FeedItemType
+} from '../../feed-items/feed-items.constants'
+import { FeedItemsService } from '../../feed-items/feed-items.service'
+import { User } from '../../users/entities/user.entity'
+import { Reward } from '../entities/reward.entity'
+import { RewardClaimedEvent } from '../events/reward-claimed.event'
+
+@Injectable()
+export class RewardClaimedListener {
+  constructor(
+    @InjectRepository(Reward)
+    private rewardRepository: Repository<Reward>,
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    private feedItemService: FeedItemsService
+  ) {}
+  @OnEvent('reward.claimed')
+  async onRewardClaimed({ rewardId, userId }: RewardClaimedEvent) {
+    const reward = await this.rewardRepository.findOne({ id: rewardId })
+    const user = await this.usersRepository.findOne({ id: userId })
+    const [_, error] = await tryAndCatch(
+      this.feedItemService.create({
+        type: FeedItemType.RewardClaimed,
+        category: FeedItemCategory.MyUpdates,
+        user,
+        reward
+      })
+    )
+    error && console.error(error)
+  }
+}
--- apps/api/src/modules/rewards/rewards.module.ts
+++ apps/api/src/modules/rewards/rewards.module.ts
@@ -6,15 +6,20 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthModule } from '../auth/auth.module'
 import { RewardsRedemption } from '../rewards-redemptions/entities/rewards-redemption.entity'
 import { User } from '../users/entities/user.entity'
+import { RewardClaimedListener } from './listeners/RewardClaimedListener'
+import { EventEmitter2 } from '@nestjs/event-emitter'
+import { FeedItemsModule } from '../feed-items/feed-items.module'
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Reward, RewardsRedemption, User]),
     // circualr dependency with user module for some reason this fixes it.
-    forwardRef(() => AuthModule)
+    forwardRef(() => AuthModule),
+    EventEmitter2,
+    FeedItemsModule
   ],
   controllers: [RewardsController],
-  providers: [RewardsService],
+  providers: [RewardsService, RewardClaimedListener],
   exports: [RewardsService]
 })
 export class RewardsModule {}
