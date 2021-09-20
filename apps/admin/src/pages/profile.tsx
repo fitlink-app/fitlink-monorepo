79351bc7a70114641d0df9bb59bfb7caaf9f6cc1
Luke Siedle <ljsiedle@gmail.com>
2021-09-20 13:59:58 +0200

    Copy tweaks

--- apps/admin/src/pages/profile.tsx
+++ apps/admin/src/pages/profile.tsx
@@ -186,15 +186,15 @@ export default function Profile() {
 
   if (!user) {
     return (
-      <Dashboard title="Profile">
+      <Dashboard title="Account Settings">
         <LoaderFullscreen />
       </Dashboard>
     )
   }
 
   return (
-    <Dashboard title="Profile">
-      <h1 className="light">Profile</h1>
+    <Dashboard title="Account Settings">
+      <h1 className="light">Account Settings</h1>
 
       {(!user.email_verified || user.email_pending) && (
         <Feedback
@@ -212,7 +212,7 @@ export default function Profile() {
       <div className="row mt-2 ai-s">
         <div className="col-12 col-md-6 col-xl-5 col-hd-4 mt-2">
           <Card className="p-3 card--stretch pb-4">
-            <h2 className="h5 color-light-grey m-0">My account settings</h2>
+            <h2 className="h5 color-light-grey m-0">My profile settings</h2>
             <form onSubmit={handleSubmit(submit)}>
               <Input
                 name="name"
