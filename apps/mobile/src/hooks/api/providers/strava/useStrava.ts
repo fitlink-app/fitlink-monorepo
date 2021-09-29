61ab0e195663a9d8a4113ea7076f3994d2817b9d
Martin <martin999b@gmail.com>
2021-09-29 09:02:50 +0200

    create todos for unlinking

--- apps/mobile/src/hooks/api/providers/strava/useStrava.ts
+++ apps/mobile/src/hooks/api/providers/strava/useStrava.ts
@@ -2,10 +2,11 @@ import {useState} from 'react';
 import {Linking} from 'react-native';
 import {InAppBrowser} from 'react-native-inappbrowser-reborn';
 import Config from 'react-native-config';
-import api from '@api';
+import api, {getErrors} from '@api';
 
 const API_URL = Config.API_URL;
 const AUTH_ENDPOINT = 'providers/strava/auth';
+const REVOKE_TOKEN_ENDPOINT = 'providers/strava/revokeToken';
 const AUTH_URL = API_URL + AUTH_ENDPOINT;
 
 export function useStrava() {
@@ -40,7 +41,9 @@ export function useStrava() {
   };
 
   const unlink = async () => {
-    // unlink
+    setLinking(true);
+    // TODO: To be implemented, API not working
+    setLinking(false);
   };
 
   return {
