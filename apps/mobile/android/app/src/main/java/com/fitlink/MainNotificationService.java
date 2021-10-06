5ac3e3976f34c095d04e3d773dc7818f11d8bc46
Martin <martin999b@gmail.com>
2021-10-06 13:18:08 +0200

    override messaging service to pipe intercom messages to its SDK

--- apps/mobile/android/app/src/main/java/com/fitlink/MainNotificationService.java
+++ apps/mobile/android/app/src/main/java/com/fitlink/MainNotificationService.java
@@ -1,16 +1,22 @@
 package com.fitlink;
 
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
+import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingService;
 import com.intercom.reactnative.IntercomModule;
+import com.google.firebase.messaging.RemoteMessage;
 
-public class MainNotificationService extends FirebaseMessagingService {
+public class MainNotificationService extends ReactNativeFirebaseMessagingService {
+
+  @Override
+  public void onNewToken(String token) {
+    IntercomModule.sendTokenToIntercom(getApplication(), token);
+    super.onNewToken(token);
+  }
 
   public void onMessageReceived(RemoteMessage remoteMessage) {
     if (IntercomModule.isIntercomPush(remoteMessage)) {
       IntercomModule.handleRemotePushMessage(getApplication(), remoteMessage);
     } else {
-      // HANDLE NON-INTERCOM MESSAGE
+      super.onMessageReceived(remoteMessage);
     }
   }
 }
