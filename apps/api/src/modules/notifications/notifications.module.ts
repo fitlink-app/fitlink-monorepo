1e0f07946ca27419b8790e310b30f70613c7f815
Luke Siedle <ljsiedle@gmail.com>
2021-10-06 14:48:02 +0200

    New secrets method for Github actions

--- apps/api/src/modules/notifications/notifications.module.ts
+++ apps/api/src/modules/notifications/notifications.module.ts
@@ -18,12 +18,20 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
+        const configCredential = configService.get<string>(
+          'FIREBASE_ADMIN_CREDENTIAL'
+        )
         const credential = admin.credential.cert(
           JSON.parse(
             Buffer.from(
-              configService.get<string>('FIREBASE_ADMIN_CREDENTIAL'),
+              configCredential
+                ? configCredential
+                : // As a work around for Lambda configuration size limits,
+                  // we need to replace this secret during the deploy process
+                  // on Github actions using sed 's/word1/word2/g'
+                  '{GITHUB_REPLACE_FIREBASE_ADMIN_CREDENTIAL}',
               'base64'
-            ).toString('utf8') //admin.credential.applicationDefault()
+            ).toString('utf8')
           )
         )
 
