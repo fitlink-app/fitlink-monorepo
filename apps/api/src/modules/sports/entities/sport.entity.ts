cd99cd8a8d6fe1a13b61806c95cd6a0333537f1a
Luke Siedle <ljsiedle@gmail.com>
2021-10-13 15:42:19 +0200

    Adds svgs and runs sport seed after migrate

--- apps/api/src/modules/sports/entities/sport.entity.ts
+++ apps/api/src/modules/sports/entities/sport.entity.ts
@@ -43,4 +43,10 @@ export class Sport extends CreatableEntity {
     nullable: true
   })
   image_url?: string
+
+  @Column({
+    type: 'text',
+    default: `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="32" height="32" fill="#ffffff" fill-opacity="0"/></svg>`
+  })
+  icon_svg: string
 }
