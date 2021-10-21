027998016ab26053ebd9e5406941860ae7135e80
Luke Siedle <ljsiedle@gmail.com>
2021-10-21 22:19:13 +0200

    Puppeteer changes

--- artifacts/.platform/hooks/postdeploy/00_migrate.sh
+++ artifacts/.platform/hooks/postdeploy/00_migrate.sh
@@ -4,5 +4,7 @@
 node migrate.js
 
 # Install the required versions of Chromium etc for Amazon Linux 2
+# Note that the environment variable is required to be set in ELB
+# PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
 sudo amazon-linux-extras install epel -y
 sudo yum install -y chromium
