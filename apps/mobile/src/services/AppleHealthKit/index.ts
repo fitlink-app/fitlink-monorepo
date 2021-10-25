f87dcaef289dac9fb77dbd2923d1c5882e81415d
Martin <martin999b@gmail.com>
2021-10-25 19:31:12 +0200

    log syncing health activities

--- apps/mobile/src/services/AppleHealthKit/index.ts
+++ apps/mobile/src/services/AppleHealthKit/index.ts
@@ -12,6 +12,7 @@ import {syncDeviceActivities, syncDeviceLifestyleData} from 'services/common';
 import {queryClient, QueryKeys} from '@query';
 import {ProviderType} from '@fitlink/api/src/modules/providers/providers.constants';
 import {parseISO} from 'date-fns';
+import Analytics from 'appcenter-analytics';
 
 export type HealthKitActivity = {
   device: string;
@@ -331,6 +332,9 @@ async function syncActivities() {
  */
 async function syncAllWithBackend() {
   try {
+    await Analytics.setEnabled(true);
+    Analytics.trackEvent('Starting sync activities');
+
     await authenticate();
     // Check if Apple Health is linked to the user
     const providers = queryClient.getQueryData(QueryKeys.MyProviders) as [];
@@ -342,6 +346,9 @@ async function syncAllWithBackend() {
       await Promise.all([syncActivities(), syncLifestyle()]);
     }
   } catch (e) {
+    Analytics.trackEvent('Syncing apple activities error', e);
+    Analytics.trackEvent('Syncing apple activities error', e.message);
+
     console.warn('Unable to sync Apple Health data with backend: ' + e);
   }
 }
