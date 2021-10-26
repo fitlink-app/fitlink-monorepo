3be191a24e6879154a3f93998c3b82b2d7fc8986
Martin <martin999b@gmail.com>
2021-10-26 12:16:50 +0200

    improve next reward query

--- apps/mobile/src/hooks/api/rewards/useNextReward.ts
+++ apps/mobile/src/hooks/api/rewards/useNextReward.ts
@@ -1,10 +1,16 @@
-import {useQuery} from 'react-query';
+import {useQuery, UseQueryOptions, QueryKey} from 'react-query';
 import {QueryKeys} from '@query';
 import api from '@api';
 import {RewardNext} from '@fitlink/api/src/modules/rewards/entities/reward.entity';
 
-export function useNextReward() {
-  return useQuery<RewardNext, Error>(QueryKeys.NextReward, () =>
-    api.get<RewardNext>('/me/next-reward'),
+export function useNextReward(
+  options?: UseQueryOptions<RewardNext, Error, RewardNext, QueryKey>,
+) {
+  return useQuery<RewardNext, Error>(
+    QueryKeys.NextReward,
+    () => api.get<RewardNext>('/me/next-reward'),
+    {
+      ...options,
+    },
   );
 }
