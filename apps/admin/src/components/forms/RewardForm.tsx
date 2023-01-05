3191c84a2191fef9ffca8fc0fe05b0b5616d0281
Valeriu Timbuc <valeriu@vtimbuc.dev>
2023-01-05 16:50:52 +0300

    update rewards redeem type #202 #223

--- apps/admin/src/components/forms/RewardForm.tsx
+++ apps/admin/src/components/forms/RewardForm.tsx
@@ -11,6 +11,7 @@ import useFormMutations from '../../hooks/api/useFormMutations'
 import { AuthContext } from '../../context/Auth.context'
 import Checkbox from '../elements/Checkbox'
 import Feedback from '../elements/Feedback'
+import { RewardRedeemType } from '@fitlink/api/src/modules/rewards/rewards.constants'
 
 export type RewardFormProps = {
   current?: Partial<RewardEntity>
@@ -34,14 +35,14 @@ const getFields = (reward: Partial<RewardEntity>) => {
     brand: reward.brand,
     limit_units: reward.limit_units,
     units_available: reward.units_available,
-    redeem_type: reward.units_available, // should be "points" or "bfit"
+    redeem_type: reward.redeem_type,
     points_required: reward.points_required,
     bfit_required: reward.bfit_required,
     image_upload: undefined
   }
 }
 
-const noop = () => {}
+const noop = () => null
 
 export default function RewardForm({
   current,
@@ -90,9 +91,9 @@ export default function RewardForm({
 
   const brand = watch('brand')
   const shortTitle = watch('name_short')
-  const type = watch('redeem_type')
-  const points = watch('points_required')
-  const bfit = watch('bfit_required')
+  const redeem_type = watch('redeem_type')
+  const points_required = watch('points_required')
+  const bfit_required = watch('bfit_required')
   const expires = watch('reward_expires_at')
   const redeemed = current.redeemed_count || 0
   const title = watch('name')
@@ -141,8 +142,12 @@ export default function RewardForm({
         image={image}
         brand={brand}
         shortTitle={shortTitle}
-        points={type === 'bfit' ? bfit : points}
-        redeemType={type}
+        points={
+          redeem_type === RewardRedeemType.BFIT
+            ? bfit_required
+            : points_required
+        }
+        redeemType={redeem_type}
         expires={expires}
         redeemed={redeemed}
         title={title}
@@ -174,23 +179,31 @@ export default function RewardForm({
       {modeRole === 'app' && (
         <div className="radio-toggles">
           <label>
-            <input type="radio" value="points" {...register('redeem_type')} />
+            <input
+              type="radio"
+              value={RewardRedeemType.Points}
+              {...register('redeem_type')}
+            />
             Points
           </label>
 
           <label>
-            <input type="radio" value="bfit" {...register('redeem_type')} />
-            $BFIT
+            <input
+              type="radio"
+              value={RewardRedeemType.BFIT}
+              {...register('redeem_type')}
+            />
+            BFIT
           </label>
         </div>
       )}
 
-      {modeRole === 'app' && type === 'bfit' ? (
+      {modeRole === 'app' && redeem_type === RewardRedeemType.BFIT ? (
         <>
           <Input
             name="bfit_required"
             placeholder="0"
-            label="$BFIT required"
+            label="BFIT required"
             type="number"
             register={register('bfit_required')}
             error={errors.bfit_required}
