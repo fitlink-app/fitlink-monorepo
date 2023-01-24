f907dcf55e030bc8bfee67d4f845cc280c04fa81
Kirill Rodichev <kirill.rodichev38@gmail.com>
2023-01-25 03:38:25 +0400

    bfit values divided by 1000

--- apps/mobile/src/utils/currency.ts
+++ apps/mobile/src/utils/currency.ts
@@ -2,3 +2,6 @@ import {BFIT_TO_USD_COEFFICIENT} from '@constants';
 
 export const convertBfitToUsd = (bfitAmount: number) =>
   bfitAmount * BFIT_TO_USD_COEFFICIENT;
+
+export const getViewBfitValue = (bfitAmount?: number) =>
+  bfitAmount !== undefined ? bfitAmount / 1000 : 0;
