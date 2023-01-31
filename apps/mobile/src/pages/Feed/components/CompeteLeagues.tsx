9ef2181fd784a0fe02bedd54b0f909c01134145c
KirillRodichev <kirill.rodichev38@gmail.com>
2023-01-31 19:23:57 +0000

    :sparkles: 344 - temporary replacement of c2e -> my leagues

--- apps/mobile/src/pages/Feed/components/CompeteLeagues.tsx
+++ apps/mobile/src/pages/Feed/components/CompeteLeagues.tsx
@@ -1,7 +1,7 @@
 import React, {FC} from 'react';
 import {useNavigation} from '@react-navigation/core';
 
-import {useCteLeagues} from '@hooks';
+import {useMyLeagues} from '@hooks';
 
 import {CteLeagueSlider} from 'components/league/CteLeagueSlider';
 import {getResultsFromPages} from 'utils/api';
@@ -14,18 +14,23 @@ interface CompeteLeaguesProps {
 export const CompeteLeagues: FC<CompeteLeaguesProps> = ({containerStyle}) => {
   const navigation = useNavigation();
 
-  const {data, fetchNextPage} = useCteLeagues();
+  // TODO: temporary solution, the API should accept additional params for my c2e leagues
+  // const {data, fetchNextPage} = useCteLeagues();
+
+  const {data, fetchNextPage} = useMyLeagues();
 
   const results = getResultsFromPages(data);
+  // TODO: temporary solution
+  const myC2ELeagues = results.filter(res => res.bfit !== undefined);
 
-  if (!results.length) {
+  if (!myC2ELeagues.length) {
     return null;
   }
 
   return (
     <View style={containerStyle}>
       <CteLeagueSlider
-        leagues={results}
+        leagues={myC2ELeagues}
         onCardPress={(id, league) =>
           navigation.navigate('League', {id, league})
         }
