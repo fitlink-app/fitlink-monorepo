ba767a96abd2b3223df556c697f50bf088e1dadf
KirillRodichev <kirill.rodichev38@gmail.com>
2023-03-09 13:33:20 +0000

    :sparkles: 549 - revert base api

--- apps/mobile/src/api/index.ts
+++ apps/mobile/src/api/index.ts
@@ -1,15 +1,17 @@
 import Axios from 'axios';
+import {store} from 'redux/store';
+import {Alert} from 'react-native';
+import Config from 'react-native-config';
+
 import {makeApi} from '@fitlink/api-sdk';
 import {ResponseError} from '@fitlink/api-sdk/types';
 import {getErrorFields, getErrorMessage} from '@fitlink/api-sdk';
-import Config from 'react-native-config';
-import {Alert} from 'react-native';
-import {store} from 'redux/store';
-import {RootState} from 'redux/reducer';
+
 import {logout} from 'redux/auth';
+import {RootState} from 'redux/reducer';
 
 const axios = Axios.create({
-  baseURL: 'https://api-prod.fitlinkapp.com/api/v1/', // Config.API_URL,
+  baseURL: Config.API_URL,
 });
 
 export type RequestError = {
