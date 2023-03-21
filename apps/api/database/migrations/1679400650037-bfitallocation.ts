a19f309332f9bb32bb19e86f0cf31bfac69f87f4
Jordan Hall <Jordan@libertyware.co.uk>
2023-03-21 12:55:04 +0000

    update migration
    Signed-off-by: Jordan Hall <Jordan@libertyware.co.uk>

--- apps/api/database/migrations/1679400650037-bfitallocation.ts
+++ apps/api/database/migrations/1679400650037-bfitallocation.ts
@@ -3,32 +3,35 @@ import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
 export class bfitallocation1679400650037 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        // 2. Add a new column to the `league` table
         await queryRunner.addColumn("league", new TableColumn({
             name: "bfitAllocation",
             type: "bigint",
             default: 0
         }));
 
-        // 3. Calculate the initial value and multiply it by 1000
         await queryRunner.query(`
             WITH earn_leagues AS (
-                SELECT id, users_count
+                SELECT *,
                 FROM league
                 WHERE access = 'competetoearn'
             ), total_users AS (
-                SELECT SUM(users_count) AS total_users_count
-                FROM earn_leagues
+                SELECT league.id AS leagueId, COUNT(user.id) AS totalUsers
+                FROM earn_leagues league
+                LEFT JOIN users user ON league.id = user.leagueId
+                GROUP BY league.id
+            ), global_total_users AS (
+                SELECT SUM(totalUsers) AS totalUsersCount
+                FROM total_users
             )
             UPDATE league
-            SET initial_value = ((earn_leagues.users_count::decimal / total_users.total_users_count::decimal * 6850) * 1000000)::bigint
-            FROM earn_leagues, total_users
+            SET bfitAllocation = ((total_users.totalUsers::decimal / global_total_users.totalUsersCount::decimal * 6850) * 100000)::bigint
+            FROM earn_leagues, total_users, global_total_users
             WHERE league.id = earn_leagues.id
+            AND league.id = total_users.leagueId
         `);
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        // Revert the changes made in the `up` method
+    public async down(queryRunner: QueryRunner): Promise<void> {+
         await queryRunner.dropColumn("league", "initial_value");
     }
 
