820aa6bec20eeec4a9e5f70dc34f002b611b8053
Jordan Hall <Jordan@libertyware.co.uk>
2023-03-21 15:07:30 +0000

    fix migration
    Signed-off-by: Jordan Hall <Jordan@libertyware.co.uk>

--- apps/api/database/migrations/1679400650037-bfitallocation.ts
+++ apps/api/database/migrations/1679400650037-bfitallocation.ts
@@ -1,38 +1,55 @@
-import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+import { LeagueAccess } from "../../src/modules/leagues/leagues.constants";
 
 export class bfitallocation1679400650037 implements MigrationInterface {
-
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.addColumn("league", new TableColumn({
-            name: "bfitAllocation",
-            type: "bigint",
-            default: 0
-        }));
+        await queryRunner.addColumn(
+            "league",
+            new TableColumn({
+                name: "bfitAllocation",
+                type: "bigint",
+                default: 0,
+            })
+        );
 
-        await queryRunner.query(`
-            WITH earn_leagues AS (
-                SELECT *,
-                FROM league
-                WHERE access = 'competetoearn'
-            ), total_users AS (
-                SELECT league.id AS leagueId, COUNT(user.id) AS totalUsers
-                FROM earn_leagues league
-                LEFT JOIN users user ON league.id = user.leagueId
-                GROUP BY league.id
-            ), global_total_users AS (
-                SELECT SUM(totalUsers) AS totalUsersCount
-                FROM total_users
-            )
-            UPDATE league
-            SET bfitAllocation = ((total_users.totalUsers::decimal / global_total_users.totalUsersCount::decimal * 6850) * 100000)::bigint
-            FROM earn_leagues, total_users, global_total_users
-            WHERE league.id = earn_leagues.id
-            AND league.id = total_users.leagueId
+        // Calculate the total number of users in all leagues with access set to 'competetoearn'
+        const totalUsersCount = await queryRunner.query(`
+        SELECT SUM(totalUsers) as totalUsersCount
+        FROM (
+            SELECT league.id AS leagueId, COUNT(user.id) AS totalUsers
+            FROM league
+            LEFT JOIN users user ON league.id = user.leagueId
+            WHERE league.access = '${LeagueAccess.competetoearn}'
+            GROUP BY league.id
+        ) AS total_users
         `);
+
+        const globalTotalUsersCount = totalUsersCount[0].totalUsersCount;
+
+        // Calculate the total number of users in each league with access set to 'competetoearn'
+        const leagueUsers = await queryRunner.query(`
+            SELECT league.id AS leagueId, COUNT(user.id) AS totalUsers
+            FROM league
+            LEFT JOIN users user ON league.id = user.leagueId
+            WHERE league.access = '${LeagueAccess.competetoearn}'
+            GROUP BY league.id
+        `);
+
+        // Calculate the allocation for each league and update the "league" table
+        for (const leagueUser of leagueUsers) {
+            const bfitAllocation =
+                ((leagueUser.totalUsers / globalTotalUsersCount) * 6850 * 100000);
+            await queryRunner.query(`
+                UPDATE league
+                SET bfitAllocation = $1
+                WHERE id = $2
+            `,
+                [bfitAllocation, leagueUser.leagueId]
+            );
+        }
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {+
-        await queryRunner.dropColumn("league", "initial_value");
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn("league", "bfitAllocation");
     }
-
 }
