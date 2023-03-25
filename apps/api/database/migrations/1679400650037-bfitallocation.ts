25e3edbf004aa215025ec41cf8232cd4a71082e0
Jordan Hall <Jordan@libertyware.co.uk>
2023-03-25 10:54:46 +0000

    fix migrations
    Signed-off-by: Jordan Hall <Jordan@libertyware.co.uk>

--- apps/api/database/migrations/1679400650037-bfitallocation.ts
+++ apps/api/database/migrations/1679400650037-bfitallocation.ts
@@ -6,62 +6,29 @@ import {
 
 export class bfitallocation1679400650037 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.addColumns('league', [
-      new TableColumn({
-        name: 'bfitAllocation',
-        type: 'numeric',
-        default: 0
-      }),
-      new TableColumn({
-        name: 'bfitWinnerPot',
-        type: 'numeric',
-        default: 0
-      })
-    ])
-
-    //comment out this part because it's failing in the update step
-    /*
-    // Calculate the total number of users in all leagues with access set to 'competetoearn'
-    const totalUsersCount = await queryRunner.query(`
-            WITH total_users AS (
-                SELECT league_users_user."leagueId",  COUNT(league_users_user."userId") as totalUsers
-                FROM league_users_user
-                INNER JOIN league ON league_users_user."leagueId" = league.id
-                WHERE league.access = 'competetoearn'
-                GROUP BY league_users_user."leagueId"
-            )
-            SELECT SUM(totalUsers) as totalUsersCount
-            FROM total_users
-        `)
-
-    const globalTotalUsersCount = totalUsersCount[0].totalUsersCount
-
-    // Calculate the total number of users in each league with access set to 'competetoearn'
-    const leagueUsers = await queryRunner.query(`
-            SELECT league_users_user."leagueId" AS leagueId, COUNT(league_users_user."userId") AS totalUsers
-            FROM league_users_user
-            INNER JOIN league ON league_users_user."leagueId" = league.id
-            WHERE league.access = 'competetoearn'
-            GROUP BY league_users_user."leagueId"
-        `)
-
-    // Calculate the allocation for each league and update the "league" table
-    for (const leagueUser of leagueUsers) {
-      const [bfitAllocation, bfitWinnerPot] = leagueBfitPots(
-        leagueUser.totalUsers,
-        globalTotalUsersCount
-      )
-      await queryRunner.query(
-        `
-                UPDATE league
-                SET bfitAllocation = $1,
-                SET bfitWinnerPot = $2
-                WHERE id = $3
-            `,
-        [bfitAllocation, bfitWinnerPot, leagueUser.leagueId]
-      )
+    if (await queryRunner.hasTable('league')) {
+      if (!await queryRunner.hasColumn('league', 'bfitAllocation')) {
+        await queryRunner.addColumn(
+          'league',
+          new TableColumn({
+            name: 'bfitAllocation',
+            type: 'numeric',
+            default: 0
+          })
+        )
+      }
+      if (!await queryRunner.hasColumn('league', 'bfitWinnerPot')) {
+        await queryRunner.addColumn(
+          'league',
+          new TableColumn({
+            name: 'bfitWinnerPot',
+            type: 'numeric',
+            default: 0
+          })
+        )
+      }
     }
-    */
+
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
