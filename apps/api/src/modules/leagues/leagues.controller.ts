d56d7038b44f4a705f0af15ae8b604fce1d9794f
Jordan Hall <Jordan@libertyware.co.uk>
2023-03-27 19:59:00 +0100

    fix: add waitlist endpoint
    Signed-off-by: Jordan Hall <Jordan@libertyware.co.uk>

--- apps/api/src/modules/leagues/leagues.controller.ts
+++ apps/api/src/modules/leagues/leagues.controller.ts
@@ -770,4 +770,14 @@ export class LeaguesController {
     )
     return leagues
   }
+
+  @Get('/leagues/:leagueId/onwaitlist')
+  @ApiTags('leagues')
+  @ApiResponse({ status: 200 })
+  async onWaitlist(
+    @Param('leagueId') leagueId: string,
+    @User() authUser: AuthenticatedUser
+  ) {
+    return this.leaguesService.onWaitlist(leagueId, authUser.id)
+  }
 }
