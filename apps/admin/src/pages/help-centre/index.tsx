571e02cff2a295a48300482144d57db5d464528f
Paul <paul@fitlinkapp.com>
2023-06-05 14:11:30 +0100

    added chatbase to help centre

--- apps/admin/src/pages/help-centre/index.tsx
+++ apps/admin/src/pages/help-centre/index.tsx
@@ -1,1 +1,18 @@
+import { useState } from 'react'
+import { useIntercom } from 'react-use-intercom'
+import Loader from '../../components/elements/Loader'
+import Dashboard from '../../components/layouts/Dashboard'
 
+export default function page() {
+
+  return (
+    <Dashboard title="Help Centre">
+      <iframe
+        src="https://www.chatbase.co/chatbot-iframe/rjhhmaF2LZeVMARWE3WJC"
+        width="100%"
+        height="700"
+        frameBorder="0"
+      />
+    </Dashboard>
+  )
+}
