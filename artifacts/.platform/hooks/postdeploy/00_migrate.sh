73f144532f44d98d08a4907d575c6f748a533308
Nick Alteen <ncalteen@github.com>
2024-01-02 14:32:11 -0500

    Fix output option

--- artifacts/.platform/hooks/postdeploy/00_migrate.sh
+++ artifacts/.platform/hooks/postdeploy/00_migrate.sh
@@ -5,6 +5,6 @@ node migrate.js
 
 # Install Chrome and symlink to Chromium (required for AL 2023)
 export BROWSERS_SRC_DIR="/usr/src/browsers" && mkdir -p $BROWSERS_SRC_DIR
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm --output $BROWSERS_SRC_DIR/google-chrome-stable_current_x86_64.rpm
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -O $BROWSERS_SRC_DIR/google-chrome-stable_current_x86_64.rpm
 sudo yum install -y -q $BROWSERS_SRC_DIR/google-chrome-stable_current_x86_64.rpm
 sudo ln -s /usr/bin/google-chrome-stable /usr/bin/chromium
